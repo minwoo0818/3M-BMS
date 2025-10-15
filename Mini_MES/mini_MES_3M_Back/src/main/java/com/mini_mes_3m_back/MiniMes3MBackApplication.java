5b0442c9d0b54534f6d8ac5c2f002039aac2c81e
minwoo0818 <145524060+minwoo0818@users.noreply.github.com>
2025-10-15 12:07:06 +0900

    백엔드 기본 세팅 업데이트

---
+++ Mini_MES/mini_MES_3M_Back/src/main/java/com/mini_mes_3m_back/MiniMes3MBackApplication.java
@@ -0,0 +1,13 @@
+package com.mini_mes_3m_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniMes3MBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniMes3MBackApplication.class, args);
+    }
+
+}
